b33823e8f7aedb8901baa8b77ba9165b584801ba
kobero98 <matteo.fede@live.it>
2022-05-25 14:54:54 +0200

    FIX proportion hope to

--- src/main/java/deliverable/Main.java
+++ src/main/java/deliverable/Main.java
@@ -5,7 +5,7 @@ import org.eclipse.jgit.api.errors.GitAPIException;
 import java.io.IOException;
 import java.text.ParseException;
 
-public class main {
+public class Main {
     public static void main(String [] args)
     {
 
